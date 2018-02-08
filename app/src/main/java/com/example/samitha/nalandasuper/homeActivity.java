7f62b648459f031ef20a122f480d800fd4655e78
Heshan <heshanwijerathne@gmail.com>
2018-02-08 21:16:31 +0530

    Test

---
+++ app/src/main/java/com/example/samitha/nalandasuper/homeActivity.java
@@ -0,0 +1,15 @@
+package com.example.samitha.nalandasuper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        }
+    }
+
